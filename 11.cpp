9c94bc3c2351dadd0790793b400b1c8873f14246
Sined Amekla <denisik566@gmail.com>
2023-05-02 19:09:19 +0300

    ccc

---
+++ 11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+//программа является учебной
+int main()
+{
+  using namespace std;
+
+  int a = 0, b = 0; // Целые числа.
+  cout << "a = ";
+  cin >> a;
+  cout << "b = ";
+  cin >> b;
+
+  cout << "(a + 2b) (a - 2b) = ";
+  cout << (a + b) * (a - b) << endl;
+cout << (a / 2b) << endl;
+  return EXIT_SUCCESS;
+}
